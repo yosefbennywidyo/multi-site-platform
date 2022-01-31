60b334078b00e39f376b75ab0a1266a12ee5e929
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-02-01 03:13:28 +0800

    Setup routes and db seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,53 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  constraints subdomain: 'buyer' do
+    resources :buyers, controller: 'buyers', type: 'Buyer' do
+      member do
+        get '/orders/new', to: 'buyers#new_order', as: 'new_order'
+        post '/orders/', to: 'buyers#create_order', as: 'create_order'
+        get '/orders/:id', to: 'buyers#show_order', as: 'show_order'
+        get '/orders/:id/edit', to: 'buyers#edit_order', as: 'edit_order'
+        put '/orders/:id', to: 'buyers#update_order', as: 'update_order'
+        delete '/orders/:id', to: 'buyers#destroy_order', as: 'destroy_order'
+      end
+    end
+    
+    root to: "buyers#home", as: :buyer_home
+  end
+
+  constraints subdomain: 'seller' do
+    resources :sellers, controller: 'sellers', type: 'Seller' do
+
+      member do
+        get '/products/new', to: 'sellers#new_product', as: 'new_product'
+        get '/products/', to: 'sellers#list_products', as: 'list_products'
+        post '/products/', to: 'sellers#create_product', as: 'create_product'
+        get '/products/:id', to: 'sellers#show_product', as: 'show_product'
+        get '/products/:id/edit', to: 'sellers#edit_product', as: 'edit_product'
+        put '/products/:id', to: 'sellers#update_product', as: 'update_product'
+        delete '/products/:id', to: 'sellers#destroy_product', as: 'destroy_product'
+      end
+
+      namespace :product_details do
+        get '/product_details/new', to: 'sellers#new_product_detail', as: 'new_product_detail'
+        post '/product_details/', to: 'sellers#create_product_detail', as: 'create_product_detail'
+        get '/product_details/:id', to: 'sellers#show_product_detail', as: 'show_product_detail'
+        get '/product_details/:id/edit', to: 'sellers#edit_product_detail', as: 'edit_product_detail'
+        put '/product_details/:id', to: 'sellers#update_product_detail', as: 'update_product_detail'
+        delete '/product_details/:id', to: 'sellers#destroy_product_detail', as: 'destroy_product_detail'
+      end
+    end
+    
+    root to: "sellers#home", as: :seller_home
+  end
+
+  resources :clients do
+    resources :transactions
+  end
+
+  get '/get_price', to: 'clients#get_price'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "clients#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+include Tenantable
+
+i = 1
+for i in 1..10 do
+  Seller.create(name: "Seller #{i}", type: "Seller", slug: "seller-#{i}", email: "seller-#{i}@test.com", password: "seller-1")
+  Buyer.create(name: "Buyer #{i}", type: "Buyer", slug: "buyer-#{i}", email: "buyer-#{i}@test.com", password: "buyer-1")
+end
+
+all_seller = Seller.all
+
+all_seller.each do |seller|
+  10.times do
+    write_to_seller_with_tenant do
+      Product.create(name: "Product by #{seller.id}", slug: "product_by_seller_#{seller.id}", seller_id: seller.id)
+    end
+  end
+end
+
+read_seller_with_tenant do
+  @product_details = Product.all.pluck(:id, :seller_id)
+end
+
+@product_details.each do |product_id, seller_id|
+  10.times do
+    write_to_seller_with_tenant do
+      ProductDetail.create(product_id: @product_details.sample[0], quantity: 1000, price: 1000, type: 'Purchase')
+    end
+  end
+end
