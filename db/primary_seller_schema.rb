6f35d12f3ca940ffe148aa06ec3bf4e8ead3597e
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-02-07 18:47:01 +0800

    Migrate Seller

--- db/primary_seller_schema.rb
+++ db/primary_seller_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_045009) do
+ActiveRecord::Schema.define(version: 2022_02_04_194722) do
 
   create_table "product_details", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "product_id", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_045009) do
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "parent_id"
     t.index ["product_id"], name: "index_product_details_on_product_id"
   end
 
