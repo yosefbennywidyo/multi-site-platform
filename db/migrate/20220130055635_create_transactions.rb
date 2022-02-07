d217bf19a6230eaf5534432afd8c571fb6495573
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2022-02-07 18:43:15 +0800

    CreateTransactions migration

--- db/migrate/20220130055635_create_transactions.rb
+++ db/migrate/20220130055635_create_transactions.rb
@@ -2,13 +2,14 @@ class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
       t.string :type # order / queue
-      t.bigint :product_id, null: false
+      t.bigint :product_id
       t.bigint :buyer_id, null: false
       t.bigint :seller_id, null: false
       t.integer :quantity
       t.integer :price
       t.string :bill_number
-      t.index [:product_id]
+      t.integer :discount
+      t.integer :total
       t.index [:buyer_id]
       t.index [:seller_id]
       t.index [:bill_number]
